d35dbc90c3e99c85c2a75ec0c82e1a56dafbbfd7
ducnam0902 <ducnam0902@gmail.com>
2022-08-14 22:32:12 +0700

    chore: update styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,22 @@
-import "./directory-item.styles.scss";
-
+import {
+  DirectoryItemContainer,
+  Body,
+  BackgroundImage,
+} from "./directory-item.styles.jsx";
+import { useNavigate } from "react-router-dom";
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="directory-item-container">
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import "./form-input.styles.scss";
+import { Group, Input, FormInputLabel } from "./form-input.styles.jsx";
 
-const FormInput = ({ label, ...otherProps }) => {
-  return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
-      {label && (
-        <label
-          className={`${
-            otherProps.value.length ? "shrink" : ""
-          } form-input-label`}
-        >
-          {label}
-        </label>
-      )}
-    </div>
-  );
-};
+const FormInput = ({ label, ...otherProps }) => (
+  <Group>
+    <Input {...otherProps} />
+    {label && (
+      <FormInputLabel shrink={otherProps.value.length}>{label}</FormInputLabel>
+    )}
+  </Group>
+);
 
 export default FormInput;
