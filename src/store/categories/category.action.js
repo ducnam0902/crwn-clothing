5775c490c7ae100afb88d59924a9bf0b5aaae23b
ducnam0902 <ducnam0902@gmail.com>
2022-08-19 21:50:10 +0700

    chore: add redux thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,3 +1,21 @@
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import {createAction} from '../../utils/reducer/reducer.utils'
 import { CATEGORIES_ACTION_TYPE } from './category,types'
-export const setCategories = (categories) => createAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES, categories);
+
+export const setCategories = (categories) => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS, categories);
+
+export const fetchCategoriesStart = () => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categories) => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,categories)
+
+export const fetchCategoriesFaild = (error) => createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED,error)
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFaild(error));
+  }
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,20 @@
 import {compose, createStore, applyMiddleware} from 'redux'
-import logger from 'redux-logger'
 
 //root-reducers
 import { rootReducer } from './root-reducer'
 
 import { persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-
+import thunk from 'redux-thunk'
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  blacklist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig,rootReducer);
 
-const middleWares = [logger];
+const middleWares = [thunk];
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
